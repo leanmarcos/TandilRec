db1be68515736f1be3c99298af6bd90f9cb5129c
leanmarcos <142643919+leanmarcos@users.noreply.github.com>
2026-04-16 12:50:02 -0300

    added enum for DB

--- backend/src/main/java/com/tandilrec/TandilRec/modules/appointments/AppointmentState.java
+++ backend/src/main/java/com/tandilrec/TandilRec/modules/appointments/AppointmentState.java
@@ -1,5 +1,20 @@
 package com.tandilrec.TandilRec.modules.appointments;
 
 public enum AppointmentState {
-    PENDING
+    ACTIVE,
+    CANCELLED,
+    PENDING ,
+    SUCCESS ,
+    FAILURE;
+
+    private String state;
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
 }
--- backend/src/main/java/com/tandilrec/TandilRec/modules/collections/CollectionResult.java
+++ backend/src/main/java/com/tandilrec/TandilRec/modules/collections/CollectionResult.java
@@ -1,4 +1,17 @@
 package com.tandilrec.TandilRec.modules.collections;
 
 public enum CollectionResult {
+    SUCCESS,
+    NOT_OPENED,
+    NOT_RECYCLABLE;
+
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result){
+        this.result = result;
+    }
 }
