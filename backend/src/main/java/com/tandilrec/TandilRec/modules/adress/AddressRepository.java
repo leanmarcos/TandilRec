aa359fc665edd9f8bb73ea4ece1dcdd8b6455f87
leanmarcos <142643919+leanmarcos@users.noreply.github.com>
2026-04-21 15:46:38 -0300

    Address Repository's new functionality (similar)

--- backend/src/main/java/com/tandilrec/TandilRec/modules/adress/AddressRepository.java
+++ backend/src/main/java/com/tandilrec/TandilRec/modules/adress/AddressRepository.java
@@ -1,7 +1,15 @@
 package com.tandilrec.TandilRec.modules.adress;
-
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface AddressRepository extends CrudRepository<Address , Long> {
+
+    @Query("SELECT a FROM Address a WHERE LOWER(a.city) = LOWER(:city) AND LOWER(a.street) = LOWER(:street) AND a.number = :number")
+    public Address searchSimilarAddress(
+            @Param("city") String city,
+            @Param("street") String street,
+            @Param("number") Integer number
+    );
 
 }
